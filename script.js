d5eecfc87e46fe20675f949ead73188aaccc67e1
7Brandyn <brandyn.evans7@gmail.com>
2023-04-28 11:55:19 -0700

    modified placements

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $('.navbar-toggle').on('click', function() {
     $('.navbar-links').slideToggle();
 });
-  
+
+$(function(){
+    $("#nav-placeholder").load("components/nav.html");
+    $("#footer-placeholder").load("components/footer.html"); 
+});
