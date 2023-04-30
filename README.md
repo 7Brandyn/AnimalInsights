d6ec52e0c67aaac4b23423df29c33fe7fd814898
7Brandyn <brandyn.evans7@gmail.com>
2023-04-30 10:34:49 -0700

    added javscript functions for team page

--- README.md
+++ README.md
@@ -5,21 +5,16 @@ Here is version 0.1 of Animal Insights' website. Future website will be made usi
 ### Completion:
 
 - \-Home (90% completed)
-- \-About (80% completed)
-- \-Team (40% completed)
+- \-About (90% completed)
+- \-Team (70% completed)
 - \-Contact (90% completed)
-- \-Projects (70% completed)
+- \-Projects (80% completed)
 - \-Terms of use (90% completed)
-- \-Privacy policy (0% completed)
-- \-Mobile Responsiveness (90% completed)
+- \-Mobile Responsiveness (95% completed)
 
 ### Todo List:
 
 - \-Team needs to showcase each individual person, a brief description about themselves, and their socials if they have them using javascript
-- \-Almost all images need to be replaced
+- \-Almost all images need to be replaced in team
 - \-Include animations
 - \-Add designated email to contact page
-- \-Add Animal Insights socials to footer
-- \-Navbar needs to be up to spec, especially with mobile
-
-\*\*Important: Images, graphics, and description will have to be provided by the rest of the team.
--- script.js
+++ script.js
@@ -11,20 +11,20 @@ var backBtn = document.getElementById("back-btn");
 backBtn.style.display === "none";
 
 function brandyn() {
+    document.querySelector("#subtitle").innerHTML = "UI Developer";
     document.querySelector("#title-swap").innerHTML = "Brandyn Evans";
-    document.querySelector("#para-swap").innerHTML = "This is a test";
-    document.querySelector("#social-icons").innerHTML = "<a href='google.com'>https://google.com</a>";
+    document.querySelector("#para-swap").innerHTML = "Hi, I’m Brandyn. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    let socialIcons = document.querySelector("#social-icons").innerHTML = "<a href='https://www.linkedin.com/in/brandyn-evans/'><img src='/icons/LinkedIn_icon.svg' alt='LinkedIn' style='width: 48px'></a>";
 
     var image = document.querySelector("#team-image");
     let teamViewer = document.querySelector("#team-viewer");
 
-    if (image.src.endsWith("teaam.png")) { // replace "original-image.png" with the current source of your image
-        image.src = "img/team/brandyn.png"; // replace "new-image.png" with the path to the image you want to replace it with
+    if (image.src.endsWith("teaam.png")) {
+        image.src = "img/team/brandyn.png";
         backBtn.style.display = "block"; 
         teamViewer.style.display = "none";
-
     } else {
-        image.src = "teaam.png";
+        image.src = "/img/teaam.png";
         backBtn.style.display = "none";
     }
 }
